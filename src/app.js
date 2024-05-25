0a9f5ca0a1cb8b78b3aa154ed23c817b74d60685
worstSolution <caitendenhonhat@gmail.com>
2024-05-25 18:12:18 +0700

    allow cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const studentRoutes = require('./routes/student');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect(process.env.NODE_ENV=='test'?'mongodb://localhost:27017/vdt2024-test':'mongodb://localhost:27017/vdt2024');
+// enable cors
+app.use(cors());
+app.options('*', cors());
 
 app.use(bodyParser.json());
 app.use('/students', studentRoutes);
