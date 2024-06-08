53277fd01bab60895ed2fa2d702059ce44dd0dbe
worstSolution <caitendenhonhat@gmail.com>
2024-06-08 21:25:13 +0700

    deved: cd workflow

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 
 const app = express();
 
-mongoose.connect(process.env.NODE_ENV=='test'?process.env.MONGODB_URI+'-test':process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_USERNAME?'mongodb://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_URI:
+    'mongodb://'+process.env.MONGODB_URI
+);
 // enable cors
 app.use(cors());
 app.options('*', cors());
