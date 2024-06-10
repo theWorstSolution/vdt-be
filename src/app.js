4bb96d86af684c9fcf7ed4b4626929e2b941e745
worstSolution <caitendenhonhat@gmail.com>
2024-06-10 10:42:25 +0700

    fixing: mongo connect string: ssl

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ console.log('MONGODB_USERNAME'+': '+process.env['MONGODB_USERNAME']);
 console.log('MONGODB_PASSWORD'+': '+process.env['MONGODB_PASSWORD']);
 console.log('PORT'+': '+process.env['PORT']);
 
-console.log(process.env.MONGODB_USERNAME?'mongodb://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_URI+'?authSource=admin':
+console.log(process.env.MONGODB_USERNAME?'mongodb://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_URI+'?authSource=admin&ssl=true':
     'mongodb://'+process.env.MONGODB_URI);
 mongoose.connect(process.env.MONGODB_USERNAME?'mongodb://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_URI:
     'mongodb://'+process.env.MONGODB_URI
