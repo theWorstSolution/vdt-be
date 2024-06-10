c8cbd5e51f2dc5fa5d0fd0382a6db831f44808e7
worstSolution <caitendenhonhat@gmail.com>
2024-06-10 10:44:45 +0700

    fixing: mongo connect string: ssl...

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ console.log('MONGODB_USERNAME'+': '+process.env['MONGODB_USERNAME']);
 console.log('MONGODB_PASSWORD'+': '+process.env['MONGODB_PASSWORD']);
 console.log('PORT'+': '+process.env['PORT']);
 
-console.log(process.env.MONGODB_USERNAME?'mongodb://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_URI+'?authSource=admin&ssl=true':
-    'mongodb://'+process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_USERNAME?'mongodb://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_URI:
+const mongoConnectString = process.env.MONGODB_USERNAME?
+    'mongodb://'+process.env.MONGODB_USERNAME+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_URI+'?authSource=admin&ssl=true':
     'mongodb://'+process.env.MONGODB_URI
-);
+console.log(mongoConnectString);
+mongoose.connect(mongoConnectString);
 // enable cors
 app.use(cors());
 app.options('*', cors());
